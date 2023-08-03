d22d56d7f8e0038309954ed33e68a7fdd2eafa62
Qiu <lopi258@outlook.com>
2023-08-03 22:44:43 +0800

    * style: removed redundant comments.

--- zephyr-bot/src/Zephyr/Packet/Login/Types.hs
+++ zephyr-bot/src/Zephyr/Packet/Login/Types.hs
@@ -3,36 +3,8 @@
 {-# OPTIONS_GHC -Wno-missing-export-lists #-}
 module Zephyr.Packet.Login.Types where
 
-import Control.Lens
 import Data.Word
 import qualified Data.ByteString.Lazy as B
-
--- data LoginError =
---     UnknownLoginError |
---     NeedCaptcha |
---     OtherLoginError |
---     UnsafeDeviceError |
---     SMSNeededError |
---     TooManySMSRequestError |
---     SMSOrVerifyNeededError |
---     SliderNeededError |
---     NoResponse
---     deriving (Eq, Show)
-
--- data LoginResponse = LoginResponse {
---     _success :: Bool,
---     _code :: Word8,
---     _login_error :: LoginError,
-
---     _captcha_image :: B.ByteString,
---     _captcha_sign :: B.ByteString,
-
---     _verify_url :: String,
---     _sms_phone :: String,
-
---     _error_message :: String
--- } deriving (Eq, Show)
--- $(makeLenses ''LoginResponse)
 
 data LoginResponse =
     LoginSuccess |
@@ -46,18 +18,3 @@ data LoginResponse =
     UnknownLoginResponse !Word8 !String
     deriving (Eq, Show)
 
-
--- defaultResponse :: LoginResponse
--- defaultResponse = LoginResponse {
---     _success = False,
---     _code = 0,
---     _login_error = UnknownLoginError,
-
---     _captcha_image = "",
---     _captcha_sign = "",
-
---     _verify_url = "",
---     _sms_phone = "",
-
---     _error_message = ""
--- }
